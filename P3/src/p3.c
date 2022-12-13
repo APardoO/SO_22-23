3975f287d777fbece26692c934c66bbb15a79a34
ApardoO <adrian.pardo.martinez@udc.es>
2022-12-13 17:24:04 +0100

    showenv terminado

--- P3/src/p3.c
+++ P3/src/p3.c
@@ -21,60 +21,77 @@
 
 extern struct cmd_data cmd_table[];
 extern struct cmd_help_data cmd_help[];
+extern char **environ;
 
 // ===== Métodos semi-privados de la práctica =====
+int print_environ_values(char *env[], char *env_type){
+	register int i=0;
+	for(i=0; environ[i]!=NULL; ++i)
+		printf("%p->%s[%d]=(%p) %s\n", &env[i], env_type, i, env[i], env[i]);
+	return SSUCC_EXIT;
+}
 
 // ==================== PRÁCTICA 3 ====================
 // [+] Hacer
-int cmdPriority(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdPriority(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
 
 // [+] Hacer
-int cmdShowvar(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdShowvar(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
 
 // [+] Hacer
-int cmdChangevar(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdChangevar(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
+	// Code
+	return SSUCC_EXIT;
+}
+
+// [✔] Hecha
+int cmdShowenv(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
+	if(lenArg==1)
+		return print_environ_values(envp, "main args3");
+	else if(strcmp(args[1], "-environ")==0)
+		return print_environ_values(environ, "environ");
+	else if(strcmp(args[1], "-addr")==0){
+		printf("environ:   %p (almacenado en %p)\n", &environ[0], &environ);
+		printf("main arg3: %p (almacenado en %p)\n", &envp[0], &envp);
+		return SSUCC_EXIT;
+	}
+
+	printf("[!] Error: %s\n", strerror(EINVAL));
+	return FSUCC_EXIT;
+}
+
+// [+] Hacer
+int cmdFork(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
 
 // [+] Hacer
-int cmdShowenv(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
-	// Code
-	return SSUCC_EXIT;
-}
-
-// [+] Hacer
-int cmdFork(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
-	// Code
-	return SSUCC_EXIT;
-}
-
-// [+] Hacer
-int cmdExecute(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdExecute(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
 
 // [!] Hacer
-int cmdListjobs(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdListjobs(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
 
 // [!] Hacer
-int cmdDeljobs(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdDeljobs(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
 
 // [!] Hacer
-int cmdJob(const int lenArg, char *args[PHARAM_LEN], List historicList, List memoryList, List processList){
+int cmdJob(const int lenArg, char *args[PHARAM_LEN], char *envp[], List historicList, List memoryList, List processList){
 	// Code
 	return SSUCC_EXIT;
 }
